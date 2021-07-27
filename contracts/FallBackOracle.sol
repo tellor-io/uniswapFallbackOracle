358d53f82222b6f2bf2d136bd24e7bb3703fcc10
cpondoc <chris.pondoc@comcast.net>
2021-07-27 09:05:27 -0400

    Add functionality for mapping of price feeds

--- contracts/FallBackOracle.sol
+++ contracts/FallBackOracle.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: Unlicense
 pragma solidity >=0.7.0;
 
 import "hardhat/console.sol";
@@ -7,8 +7,11 @@ import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 
 contract FallBackOracle is UsingTellor {
 
-    // Pool to create
+    // Defining Uniswap Pool
     IUniswapV3Pool uniswapPool;
+
+    // Mapping for Data IDs and Uniswap Contract Addresses
+    mapping(uint => address) public priceFeeds;
 
     // Grabs current Tellor value, as well as the timestamp retrieved
     function grabTellorValue(uint256 _dataId) internal view returns (uint256, uint256) {
@@ -18,20 +21,27 @@ contract FallBackOracle is UsingTellor {
     }
 
     // Grab the value from Uniswap and the second difference
-    function grabUniswapValue(uint32[] memory testValues) external view returns (uint160[] memory) {
+    function grabUniswapValue(uint _dataId, uint32[] memory testValues) external returns (uint160) {
+      uniswapPool = IUniswapV3Pool(address(priceFeeds[_dataId]));
       (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s) = uniswapPool.observe(testValues);
       int56 tickDifference = (tickCumulatives[0] - tickCumulatives[1]);
       uint160 secondDifference = (secondsPerLiquidityCumulativeX128s[0] - secondsPerLiquidityCumulativeX128s[1]);
-      return secondsPerLiquidityCumulativeX128s;
+      return secondDifference;
     }
 
+    // General function grab new values from the oracle
     function grabNewValue(uint256 _dataId) external view returns (uint256) {
       // Retrieve Tellor Value
       (uint256 tellorValue, uint256 tellorTimestamp) = grabTellorValue(_dataId);
       return tellorValue;
     }
 
-    constructor(address payable _tellorAddress, address _poolAddress) UsingTellor(_tellorAddress) public {
-      uniswapPool = IUniswapV3Pool(_poolAddress);
+    // Sets up respective addresses, and also creates the mapping of price feeds
+    constructor(address payable _tellorAddress, uint[] memory dataIds, address[] memory contracts) 
+    UsingTellor(_tellorAddress) public {
+      require(dataIds.length == contracts.length, "Data IDs and Contracts are not same length");
+      for (uint i = 0; i < dataIds.length; i++) {
+        priceFeeds[dataIds[i]] = contracts[i];
+      }
     }
 }
--- test/testFallBackOracle.js
+++ test/testFallBackOracle.js
@@ -1,38 +1,33 @@
 const { expect } = require("chai");
 const { abi, bytecode} = require('@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json');
 const { ethers } = require("hardhat");
+const BN = require('bn.js');
 
 // Addresses for Main Functions
 const tellorAddress = "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0";
 const sampleUniswapAddress = "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8";
 
+// Sample variables for data IDs and contracts
+const IDs = [1]
+const contractAddresses = [sampleUniswapAddress]
+
 describe("Fallback Oracle", function() {
-  let usingTellor;
-  let uniswapFactory;
 
-  // Set up all pertinent libraries
+  // Set up FallBack Oracle Contract
   beforeEach(async function() {
-
-    // Set up Uniswap Factory and Pool
-    let UniswapFactory = await ethers.getContractFactory(abi, bytecode);
-    uniswapFactory = await UniswapFactory.deploy();
-    await uniswapFactory.deployed();
-
-    let UsingTellor = await ethers.getContractFactory("FallBackOracle");
-    //usingTellor = await UsingTellor.deploy(tellorOracle.address, "0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8");
-    usingTellor = await UsingTellor.deploy(tellorAddress, sampleUniswapAddress);
-    await usingTellor.deployed();
-
+    let FallBackOracle = await ethers.getContractFactory("FallBackOracle");
+    fallBackOracle = await FallBackOracle.deploy(tellorAddress, IDs, contractAddresses);
+    await fallBackOracle.deployed();
   });
 
   // Use Tellor Playground to update values and check both the mapping and 
   // return function
   it("Check for Tellor Update Values", async function() {
-    const firstVal = (await usingTellor.grabNewValue(1));
-    const secondVal = (await usingTellor.grabNewValue(2));
+    const firstVal = (await fallBackOracle.grabNewValue(1));
+    const secondVal = (await fallBackOracle.grabNewValue(2));
     console.log(firstVal);
     console.log(secondVal);
-    const ticks = await usingTellor.grabUniswapValue([0, 20]);
+    const ticks = await fallBackOracle.grabUniswapValue(1, [0, 20]);
     console.log(ticks);
   });
 });
