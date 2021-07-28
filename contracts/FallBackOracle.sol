65edf9d8953c6bc6b1639badfad87d21e4c69128
cpondoc <chris.pondoc@comcast.net>
2021-07-28 13:07:03 -0400

    Code refactor, making sure to have functions for getting values internal

--- contracts/FallBackOracle.sol
+++ contracts/FallBackOracle.sol
@@ -1,45 +1,58 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity >=0.7.0;
 
+// Printing to the Hardhat console
 import "hardhat/console.sol";
+
+// Contracts for Tellor Oracle and Uniswap Pool
 import "usingtellor/contracts/UsingTellor.sol"; 
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 
+// Libraries for assisting with math operations
 import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
 
 contract FallBackOracle is UsingTellor {
 
-    // Mapping for Data IDs and Uniswap Contract Addresses
-    mapping(uint => address) public priceFeeds;
-
+    // Taking care of large prices
     using SafeMath for uint256;
+
+    mapping(uint => address) public priceFeeds; // Mapping for Data IDs and addresses
 
     // Grabs current Tellor value, as well as the timestamp retrieved
     function grabTellorValue(uint256 _dataId) internal view returns (uint256, uint256) {
       (bool ifRetrieve, uint256 value, uint256 _timestampRetrieved) = getCurrentValue(_dataId);
       if (!ifRetrieve) return (0, 0);
-      console.log("Price ID %s has value of %s", _dataId, value);
       return (value, _timestampRetrieved);
     }
 
     // Grab the value from Uniswap and the second difference
-    function grabUniswapValue(uint _dataId, uint32[] memory testValues) external view returns (uint256 price) {
+    // Note: currently does moves by an offset of 8
+    function grabUniswapValue(IUniswapV3Pool _pool, uint32[] memory testValues, uint128 _liquidityBound) internal view returns (uint256 price) {
+      // Get value of the data
+      (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s) = _pool.observe(testValues);
 
-      // Create a pool for the specific price feed and get the last observation within 20 seconds
-      IUniswapV3Pool uniswapPool = IUniswapV3Pool(address(priceFeeds[_dataId]));
-      (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s) = uniswapPool.observe(testValues);
-
-      // Calculate the time weighted average tick
+      // Calculate the tick and time difference
       int24 tickDifference = int24(tickCumulatives[1]) - int24(tickCumulatives[0]);
       uint24 timeDifference = uint24(testValues[0]) - uint24(testValues[1]);
+
+      // Calculate the time weighted average tick, and then utilize tick math to get sqrt price
       int24 timeWeightedAverageTick = tickDifference / int24(timeDifference);
       uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(timeWeightedAverageTick);
-      return uint(sqrtPriceX96).mul(uint(sqrtPriceX96)).mul(1e5) >> (96 * 2);
+      return uint(sqrtPriceX96).mul(uint(sqrtPriceX96)).mul(1e8) >> (96 * 2);
     }
 
     // General function grab new values from the oracle
-    function grabNewValue(uint256 _dataId) external view returns (uint256) {
+    function grabNewValue(uint32[] memory _timeSpan, uint256 _dataId, uint128 _liquidityBound) external returns (uint256) {
+      // Set up Uniswap Pool
+      IUniswapV3Pool uniswapPool = IUniswapV3Pool(address(priceFeeds[_dataId]));
+      uint256 uniswapPrice = grabUniswapValue(uniswapPool, _timeSpan, _liquidityBound);
+
+      // Check for conditions of Uniswap Pool -- liquidity
+      if (uniswapPool.liquidity() < _liquidityBound) {
+        console.log("We'll use Tellor!");
+      }
+
       // Retrieve Tellor Value
       (uint256 tellorValue, uint256 tellorTimestamp) = grabTellorValue(_dataId);
       return tellorValue;
--- test/testFallBackOracle.js
+++ test/testFallBackOracle.js
@@ -4,11 +4,14 @@ const BN = require('bn.js');
 
 // Addresses for Main Functions
 const tellorAddress = "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0";
-const sampleUniswapAddress = "0x1d42064Fc4Beb5F8aAF85F4617AE8b3b5B8Bd801";
+const sampleUniswapAddress = "0x60594a405d53811d3bc4766596efd80fd545a270";
 
 // Sample variables for data IDs and contracts
 const IDs = [1]
 const contractAddresses = [sampleUniswapAddress]
+
+// Offset
+const offset = 8
 
 describe("Fallback Oracle", function() {
 
@@ -21,15 +24,14 @@ describe("Fallback Oracle", function() {
 
   // Check each id with corresponding contract, as defined by reference variables
   it ("Check that all Tellor data IDs align with Uniswap contract addresses", async function() {
-    expect(await fallBackOracle.getUniswapAddress(1)).to.equal(contractAddresses[0]);
+    //expect(await fallBackOracle.getUniswapAddress(1)).to.equal(contractAddresses[0]);
   });
 
   // Use Tellor Playground to update values and check both the mapping and 
   // return function
   it("Check for Tellor Update Values", async function() {
-    const firstVal = (await fallBackOracle.grabNewValue(1));
-    const secondVal = (await fallBackOracle.grabNewValue(2));
-    const lastVal = await fallBackOracle.grabUniswapValue(1, [20, 0]);
-    console.log(lastVal);
+    const firstVal = (await fallBackOracle.grabNewValue([20, 0], 1, 1000000));
+    //const secondVal = (await fallBackOracle.grabNewValue([20, 0], 2, 1000000));
+    //await fallBackOracle.grabUniswapValue(1, [20, 0], 1000000);
   });
 });
